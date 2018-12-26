49cbfe19f2563719fa94433c3d85ae17ae1d9548
flylib <liushaomingdev@163.com>
2018-12-26 17:55:30 +0800

    设置app-common的css样式

--- fpush-admin/js/info.js
+++ fpush-admin/js/info.js
@@ -34,20 +34,57 @@ var app = new Vue({
             });
         },
         push: function push() {
-            app.dialogFormVisible = false;
-            axios.post(ctxPath + '/app/push', app.pushForm).then(function (response) {
-                console.log(response);
-                this.$message({
-                    message: '恭喜你，这是一条成功消息',
-                    type: 'success',
-                    center: true
+            var pass = validatePushForm();
+            if (pass) {
+                app.dialogFormVisible = false;
+                axios.post(ctxPath + '/app/push', app.pushForm).then(function (response) {
+                    console.log(response);
+                    app.$message({
+                        message: '发送即时消息成功',
+                        type: 'success',
+                        center: true
+                    });
+                }).catch(function (error) {
+                    console.log(error);
                 });
-            }).catch(function (error) {
-                console.log(error);
-            });
+            }
         }
     }
 });
+
+function validatePushForm() {
+    var pass = true;
+    var errorMsg = "";
+
+    app.pushForm.receiverAlias
+
+    if (app.pushForm.desc == undefined || app.pushForm.desc == "") {
+        pass = false;
+        errorMsg = "文本内容为空";
+    }
+
+    if (app.pushForm.title == undefined || app.pushForm.title == "") {
+        pass = false;
+        errorMsg = "标题为空";
+    }
+
+    if (app.pushForm.receiverAlias == undefined || app.pushForm.receiverAlias == "") {
+        pass = false;
+        errorMsg = "接收用户名为空";
+    }
+
+
+
+    if (!pass) {
+        app.$message({
+            message: errorMsg,
+            type: 'error',
+            center: true
+        });
+    }
+
+    return pass;
+}
 
 app.getTable();
 
